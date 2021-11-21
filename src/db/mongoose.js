1c78a6f86d4976012cf78cedc1179a1dc463d558
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-21 19:41:45 +0530

    new project

---
+++ src/db/mongoose.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+})
+
+// const Tasks = mongoose.model('Tasks', {
+//     description: {
+//         type:String,
+//         trim:true,
+//         required:true
+//     },
+//     completed: {
+//         type:Boolean,
+//         default:false
+//     }
+// })
+// const task = new Tasks({
+//     description: '  meeting  ',
+
+// })
+// task.save().then((res) => {
+//     console.log(res)
+// }).catch((err) => {
+//     console.log(err)
+// })
