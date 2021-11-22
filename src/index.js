da444e3010e3be27b803b1897dac782162001c88
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-22 07:50:43 +0530

    delete user router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("./db/mongoose")
 const User = require("./models/user")
 const Task = require("./models/task")
+const { findByIdAndDelete } = require("./models/user")
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -58,6 +59,15 @@ app.patch("/users/:id", async (req, res) => {
     res.send(user)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    !user ? res.status(404).send("Oops no user for this id") : res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
   }
 })
 
