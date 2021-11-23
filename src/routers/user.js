c071b8140cb8b3f8b2e4b9ccfed57433482ea538
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-23 14:33:12 +0530

    logout all

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,14 @@
 const express = require("express")
-const router = new express.Router()
-const auth = require("../middleware/auth")
 const User = require("../models/user")
+const auth = require("../middleware/auth")
+const router = new express.Router()
+
 router.post("/users", async (req, res) => {
   const user = new User(req.body)
+
   try {
     await user.save()
     const token = await user.generateAuthToken()
-
     res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
@@ -18,7 +19,6 @@ router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
     const token = await user.generateAuthToken()
-
     res.send({ user, token })
   } catch (e) {
     res.status(400).send()
@@ -31,24 +31,40 @@ router.post("/users/logout", auth, async (req, res) => {
       return token.token !== req.token
     })
     await req.user.save()
+
     res.send()
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
+  }
+})
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+    await req.user.save()
+    res.send()
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user)
 })
+
 router.get("/users/:id", async (req, res) => {
   const _id = req.params.id
 
   try {
     const user = await User.findById(_id)
 
-    !user ? res.status(404).send() : res.send(user)
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
   }
 })
 
@@ -58,16 +74,19 @@ router.patch("/users/:id", async (req, res) => {
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update),
   )
+
   if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates" })
+    return res.status(400).send({ error: "Invalid updates!" })
   }
+
   try {
     const user = await User.findById(req.params.id)
+
     updates.forEach((update) => (user[update] = req.body[update]))
     await user.save()
 
     if (!user) {
-      return res.status(404).send("Oops user not found")
+      return res.status(404).send()
     }
 
     res.send(user)
@@ -79,9 +98,14 @@ router.patch("/users/:id", async (req, res) => {
 router.delete("/users/:id", async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id)
-    !user ? res.status(404).send("Oops no user for this id") : res.send(user)
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
   }
 })
 
