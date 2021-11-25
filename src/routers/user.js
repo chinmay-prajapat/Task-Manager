1b33c9130d23776ac3255c2ecbd7fc440e6ae803
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-25 21:39:02 +0530

    crop picture

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,7 @@ const express = require("express")
 const User = require("../models/user")
 const auth = require("../middleware/auth")
 const multer = require("multer")
-
+const sharp = require("sharp")
 const router = new express.Router()
 
 router.post("/users", async (req, res) => {
@@ -121,7 +121,11 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
   },
@@ -141,7 +145,7 @@ router.get("/users/:id/avatar", async (req, res) => {
     if (!user || !user.avatar) {
       throw new Error()
     }
-    res.set("Content-Type", "image/jpg")
+    res.set("Content-Type", "image/png")
     res.send(user.avatar)
   } catch (e) {
     res.status(400).send(e)
