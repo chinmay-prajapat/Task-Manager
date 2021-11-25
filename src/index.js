08fcda662d43d01b0688ff596e285a8c732eafe4
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-25 21:19:26 +0530

    refactored

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require("express")
 require("./db/mongoose")
 const userRouter = require("./routers/user")
 const taskRouter = require("./routers/task")
-const { json } = require("stream/consumers")
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => console.log("Server is up on port" + port))
 // app.use((req, res, next) => {
 //   if (req.method === "GET") {
 //     res.send("Get requests are disabled")
@@ -17,36 +21,3 @@ const port = process.env.PORT || 3000
 // app.use((req, res) =>
 //   res.status(503).send("Ooops no operations can be perform at this time"),
 // )
-
-const multer = require("multer")
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error("Please upload a word doc"))
-    }
-    cb(undefined, true)
-    // cb(new Error("File must be a PDF"))
-    // cb(undefine, true)
-    // cb(undefined, false)
-  },
-})
-app.post(
-  "/upload",
-  upload.single("upload"),
-  (req, res) => {
-    res.send()
-  },
-  (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-  },
-)
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-app.listen(port, () => console.log("Server is up on port" + port))
