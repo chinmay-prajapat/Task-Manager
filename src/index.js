408933d4afe07f151ca5fd94c904455be4e0c0e5
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-25 14:03:12 +0530

    limiting file size to be uploaded

--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ const port = process.env.PORT || 3000
 const multer = require("multer")
 const upload = multer({
   dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    cb(new Error("File must be a PDF"))
+    cb(undefine, true)
+    cb(undefined, false)
+  },
 })
 app.post("/upload", upload.single("upload"), (req, res) => {
   res.send()
