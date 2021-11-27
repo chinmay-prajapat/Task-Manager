08037ccdb349f41f2c65bfffc240e8a2d1230576
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-27 07:29:59 +0530

    send email

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const User = require("../models/user")
 const auth = require("../middleware/auth")
 const multer = require("multer")
 const sharp = require("sharp")
-const { sendWelcomeEmail, deleteEmail } = require("../emails/account")
+const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account")
 const router = new express.Router()
 
 router.post("/users", async (req, res) => {
@@ -98,7 +98,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove()
-    deleteEmail(req.user.email, req.user.name)
+    sendCancelationEmail(req.user.email, req.user.name)
 
     res.send(req.user)
   } catch (e) {
