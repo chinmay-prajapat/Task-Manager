51528487b7284b215fe4a815540ce97aa80b1370
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-27 11:50:55 +0530

    fixed tasks

--- src/routers/temp.js
+++ src/routers/temp.js
@@ -8,75 +8,83 @@ router.post("/tasks", auth, async (req, res) => {
     ...req.body,
     owner: req.user._id,
   })
+
   try {
-    const tasks = await task.save()
-    res.status(201).send(tasks)
+    await task.save()
+    res.status(201).send(task)
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   const match = {}
   const sort = {}
+
   if (req.query.completed) {
     match.completed = req.query.completed === "true"
   }
+
   if (req.query.sortBy) {
     const parts = req.query.sortBy.split(":")
     sort[parts[0]] = parts[1] === "desc" ? -1 : 1
   }
-  try {
-    // const tasks = await Task.find({})
-    // const task = await Task.find({ owner: req.user._id })
-    await req.user.populate({
-      path: "tasks",
-      match,
-      options: {
-        limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip),
-        sort,
-        // sort: {
-        //   // createdAt: 1,
-        //   // completed: -1, //this will give us completed task first
-        // },
-      },
-    })
 
+  try {
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate()
     res.send(req.user.tasks)
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
   }
 })
 
 router.get("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id
+
   try {
-    // const task = await Task.findById(req.params.id)
     const task = await Task.findOne({ _id, owner: req.user._id })
+
     if (!task) {
-      res.status(404).send("Task not found")
+      return res.status(404).send()
     }
+
     res.send(task)
   } catch (e) {
     res.status(500).send()
   }
 })
+
 router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ["description", "completed"]
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update),
   )
+
   if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates" })
+    return res.status(400).send({ error: "Invalid updates!" })
   }
+
   try {
     const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
     if (!task) {
-      return res.status(404).send("Oops user not found")
+      return res.status(404).send()
     }
+
     updates.forEach((update) => (task[update] = req.body[update]))
     await task.save()
     res.send(task)
@@ -91,9 +99,15 @@ router.delete("/tasks/:id", auth, async (req, res) => {
       _id: req.params.id,
       owner: req.user._id,
     })
-    !task ? res.status(404).send("no task found!") : res.send(task)
+
+    if (!task) {
+      res.status(404).send()
+    }
+
+    res.send(task)
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send()
   }
 })
+
 module.exports = router
